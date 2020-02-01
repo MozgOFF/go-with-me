c9dc6b999fb20d59a971be0e4b144f371c1c368f
4kuanysh <4kuanish@gmail.com>
2020-02-01 19:24:06 +0600

    jwt djoser auth

--- gowithme/settings.py
+++ gowithme/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +102,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
