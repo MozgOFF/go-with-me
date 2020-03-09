3accd6eefd6d43b78a28c355f09d8977ffb7bfc9
4kuan <4kuanish@gmail.com>
2020-03-09 14:35:30 +0600

    removed djoser

--- gowithme/urls.py
+++ gowithme/urls.py
@@ -18,8 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/auth/', include('djoser.urls')),
-    path('api/v1/auth/', include('djoser.urls.jwt')),
     path('api/v1/account/', include('account.urls')),
     path('api/v1/event/', include('event.urls')),
 ]
