010dd4465f96ba179acd4dd394e8b411737aa416
Kuanysh <4kuanish@gmail.com>
2020-03-14 10:31:50 +0600

    WIP Event models

--- event/serializers.py
+++ event/serializers.py
@@ -1,30 +1,30 @@
-from rest_framework import serializers
-from event.models import Event
+# from rest_framework import serializers
+# from event.models import Event
 
 
-class EventDetailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Event
-        fields = '__all__'
+# class EventDetailSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Event
+#         fields = '__all__'
 
 
-class EventCreateSerializer(serializers.ModelSerializer):
-    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+# class EventCreateSerializer(serializers.ModelSerializer):
+#     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
 
-    class Meta:
-        model = Event
-        fields = '__all__'
-
-
-class EventUpdateDetailSerializer(serializers.ModelSerializer):
-    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
-
-    class Meta:
-        model = Event
-        fields = ('event_brief_description', 'event_title', 'event_date', 'event_type',)
+#     class Meta:
+#         model = Event
+#         fields = '__all__'
 
 
-class EventArchiveSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Event
-        fields = ('event_state',)
+# class EventUpdateDetailSerializer(serializers.ModelSerializer):
+#     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+#     class Meta:
+#         model = Event
+#         fields = ('event_brief_description', 'event_title', 'event_date', 'event_type',)
+
+
+# class EventArchiveSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Event
+#         fields = ('event_state',)
--- event/views.py
+++ event/views.py
@@ -1,30 +1,30 @@
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
-from event.serializers import EventDetailSerializer, EventCreateSerializer, EventUpdateDetailSerializer, \
-    EventArchiveSerializer
-from .models import Event
+# from rest_framework import generics
+# from rest_framework.permissions import IsAuthenticated
+# from event.serializers import EventDetailSerializer, EventCreateSerializer, EventUpdateDetailSerializer, \
+#     EventArchiveSerializer
+# from .models import Event
 
 
-class EventCreateView(generics.CreateAPIView):
-    permission_classes = (IsAuthenticated,)
-    serializer_class = EventCreateSerializer
+# class EventCreateView(generics.CreateAPIView):
+#     permission_classes = (IsAuthenticated,)
+#     serializer_class = EventCreateSerializer
 
 
-class EventListView(generics.ListAPIView):
-    serializer_class = EventDetailSerializer
-    queryset = Event.objects.all()
+# class EventListView(generics.ListAPIView):
+#     serializer_class = EventDetailSerializer
+#     queryset = Event.objects.all()
 
 
-class EventDetailView(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = EventDetailSerializer
-    queryset = Event.objects.all()
+# class EventDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     serializer_class = EventDetailSerializer
+#     queryset = Event.objects.all()
 
 
-class EventDetailUpdate(generics.UpdateAPIView):
-    permission_classes = (IsAuthenticated,)
-    serializer_class = EventDetailSerializer
+# class EventDetailUpdate(generics.UpdateAPIView):
+#     permission_classes = (IsAuthenticated,)
+#     serializer_class = EventDetailSerializer
 
 
-class EventArchive(generics.UpdateAPIView):
-    permission_classes = (IsAuthenticated,)
-    serializer_class = EventArchiveSerializer
+# class EventArchive(generics.UpdateAPIView):
+#     permission_classes = (IsAuthenticated,)
+#     serializer_class = EventArchiveSerializer
